cf08559318e0396bfab69c11da2ed061cce22be4
Asingo Paul <paulasingo32@gmail.com>
2025-08-07 11:09:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,28 +3,28 @@ from flask import Flask, render_template, request, redirect, flash, url_for
 from flask_mail import Mail, Message
 import os
 
-app = Flask(__name__)
-app.secret_key = 'your_secret_key'
+application = Flask(__name__)
+application.secret_key = 'your_secret_key'
 
 
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
-app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD') 
+application.config['MAIL_SERVER'] = 'smtp.gmail.com'
+application.config['MAIL_PORT'] = 587
+application.config['MAIL_USE_TLS'] = True
+application.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+application.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD') 
 
-mail = Mail(app)
+mail = Mail(application)
 
-@app.route('/')
+@application.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/projects')
+@application.route('/projects')
 def projects():
     return render_template('projects.html')
 
 
-@app.route('/contact', methods=['GET', 'POST'])
+@application.route('/contact', methods=['GET', 'POST'])
 def contact():
     if request.method == 'POST':
         name = request.form['name']
@@ -51,4 +51,4 @@ def contact():
 
 port = int(os.environ.get("PORT", 5000))
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=port)
+    application.run(host="0.0.0.0", port=port)
